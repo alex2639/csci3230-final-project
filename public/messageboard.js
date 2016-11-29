24f9440ea9a379284d0e0df6986803508a985cfb
saifniaz <saifniaz.3@gmail.com>
2016-11-28 20:53:54 -0800

    Updated nav bar

--- public/messageboard.js
+++ public/messageboard.js
@@ -1,5 +1,6 @@
 //Take input to later be outputted as part of the messages 
 function myFunction() {
     var x = document.getElementById("comment").value;
-    document.getElementById("demo").innerHTML = x;
+    var y = x.split(" ");
+    document.getElementById("demo").innerHTML = y[1];
 }
