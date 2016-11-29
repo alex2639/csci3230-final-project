12a94c9db3c432a6984a8d68e8b9705eef0c7a0d
Agilan19 <swagilan19@gmail.com>
2016-11-28 22:21:25 -0500

    Adding in messageboard js and css files

---
+++ public/messageboard.js
@@ -0,0 +1,5 @@
+//Take input to later be outputted as part of the messages 
+function myFunction() {
+    var x = document.getElementById("comment").value;
+    document.getElementById("demo").innerHTML = x;
+}
